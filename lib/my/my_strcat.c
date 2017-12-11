20e5f1a8f2606ab5930e220bb73d01b5edf4466f
Sanchez <lucas.sanchez@epitech.eu>
2017-12-11 14:31:04 +0100

    Norm correction

--- lib/my/my_strcat.c
+++ lib/my/my_strcat.c
@@ -14,7 +14,7 @@ char *my_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 	char *str =
-		malloc(sizeof(char) * (my_strlen(dest) + my_strlen(src) + 1));
+	malloc(sizeof(char) * (my_strlen(dest) + my_strlen(src) + 1));
 
 	while (dest[i] != 0) {
 		str[i] = dest[i];
